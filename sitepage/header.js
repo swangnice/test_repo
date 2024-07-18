1b84e56527c677ec3e37be5483c389aa27c17302
Shibo Wang <s.wang20010618@gmail.com>
2024-07-18 23:07:56 +0800

    Example Repo Prototype

--- sitepage/header.js
+++ sitepage/header.js
@@ -2,13 +2,14 @@
 function loadHeader() {
     const headerHTML = `
     <header>
-        <h1>欢迎来到我的博客</h1>
+        <h1>SWangNice</h1>
     </header>
     <nav>
         <ul>
-            <li><a href="#about">关于我</a></li>
-            <li><a href="#blog">博客文章</a></li>
-            <li><a href="#contact">联系方式</a></li>
+            <li><a href="https://swangnice.github.io/">Home</a></li>
+            <li><a href="https://swangnice.github.io/#blog">Projects</a></li>
+            <li><a href="https://swangnice.github.io/#contact">Contact</a></li>
+            <li><a href="https://swangnice.github.io/#about">About Me</a></li>
         </ul>
     </nav>
     `;
